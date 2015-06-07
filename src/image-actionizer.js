96238b6ea7481d3d31ac75f881e42527001d26b0
Andrew Kos <akos123@gmail.com>
2015-06-07 13:32:33 -0500

    No more angular

--- src/image-actionizer.js
+++ src/image-actionizer.js
@@ -1,15 +1,10 @@
-(function ($, Path2D) {
+var ImageActionizer = (function ($, Path2D, CustomEvent) {
   'use strict';
 
-  var drawingModes = {
-    DOTS: 'dots',
-    FREE: 'free'
-  };
-
-  var getCursorPosition = function($event) {
+  var getCursorPosition = function($event, $canvas) {
     var x;
     var y;
-    var canoffset = this.$canvas.offset();
+    var canoffset = $canvas.offset();
 
     return {
       x: event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft - Math.floor(canoffset.left),
@@ -22,102 +17,72 @@
     this.$canvas = $(canvas);
     this.context = canvas.getContext('2d');
 
-    this.drawMode = drawingModes.DOTS;
-
     this.currPath = null;
+    this.inPath = null;
     this.drawingLine = false;
 
     this.paths = [];
-    this.dots = [];
+
+    this.bindEvents();
+  };
+
+  ImageActionizer.prototype.bindEvents = function () {
+    this.canvas.addEventListener('mousedown', this.startFreeDraw.bind(this));
+    this.canvas.addEventListener('mousemove', this.doFreeDraw.bind(this));
+    this.canvas.addEventListener('mouseup', this.stopFreeDraw.bind(this));
   };
 
   ImageActionizer.prototype.startFreeDraw = function (event) {
-    if (this.drawMode === drawingModes.FREE) {
-      var pos = getCursorPosition(event);
-      this.currPath = new Path2D();
-      this.currPath.moveTo(pos.x, pos.y);
-      this.drawingLine = true;
-    }
+    var pos = getCursorPosition(event, this.$canvas);
+    this.currPath = new Path2D();
+    this.currPath.moveTo(pos.x, pos.y);
+    this.drawingLine = true;
   };
 
   ImageActionizer.prototype.doFreeDraw = function (event) {
-    var pos = getCursorPosition(event);
-    this.currPath = -1;
-    for (var i = 0; i < $scope.paths.length; i++) {
-      if (context.isPointInPath($scope.paths[i], pos.x, pos.y)) {
-        $scope.inPath = i;
+    var pos = getCursorPosition(event, this.$canvas);
+    var oldPath = this.inPath;
+
+    this.inPath = -1;
+    for (var i = 0; i < this.paths.length; i++) {
+      if (this.context.isPointInPath(this.paths[i], pos.x, pos.y)) {
+        this.inPath = i;
       }
     }
 
-    if ($scope.drawMode === $scope.drawingModes.FREE && $scope.drawingLine) {
-      currPath.lineTo(pos.x, pos.y);
-      context.stroke(currPath);
+    if (oldPath !== this.inPath) {
+      // fire event to say that path has changed
+      var pathChangedEvent = new CustomEvent(
+        'ImageActionizer:PathChanged', {
+          detail: {
+            oldPath: oldPath,
+            newPath: this.inPath
+          }
+        });
+      this.canvas.dispatchEvent(pathChangedEvent);
     }
+
+    if (this.drawingLine) {
+      this.currPath.lineTo(pos.x, pos.y);
+      this.context.stroke(this.currPath);
+    }
+  };
+
+  ImageActionizer.prototype.stopFreeDraw = function (event) {
+    if (this.drawingLine) {
+      this.currPath.closePath();
+      this.context.fillStyle = 'rgba(255, 0, 0, 0.5)';
+      this.context.fill(this.currPath);
+      this.context.stroke(this.currPath);
+      this.drawingLine = false;
+      this.paths.push(this.currPath);
+    }
+  };
+
+  ImageActionizer.prototype.clearCanvas = function () {
+    this.paths = [];
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   };
 
   return ImageActionizer;
-})(jQuery, Path2D);
-
-dotsApp.controller('DotsController', function ($scope) {
-
-  $scope.drawMode = $scope.drawingModes.DOTS;
-
-  $scope.inPath = -1;
-  $scope.drawingLine = false;
-  $scope.paths = [];
-  var currPath;
-  $scope.startFreeDraw = function ($event) {
-    if ($scope.drawMode === $scope.drawingModes.FREE) {
-      var pos = getCursorPosition($event);
-      currPath = new Path2D();
-      currPath.moveTo(pos.x, pos.y);
-      $scope.drawingLine = true;
-    }
-  };
-  $scope.doFreeDraw = function ($event) {
-    var pos = getCursorPosition($event);
-    $scope.inPath = -1;
-    for (var i = 0; i < $scope.paths.length; i++) {
-      if (context.isPointInPath($scope.paths[i], pos.x, pos.y)) {
-        $scope.inPath = i;
-      }
-    }
-
-    if ($scope.drawMode === $scope.drawingModes.FREE && $scope.drawingLine) {
-      currPath.lineTo(pos.x, pos.y);
-      context.stroke(currPath);
-    }
-  };
-  $scope.stopFreeDraw = function ($event) {
-    if ($scope.drawMode === $scope.drawingModes.FREE && $scope.drawingLine) {
-      currPath.closePath();
-      context.fillStyle = 'rgba(255, 0, 0, 0.5)';
-      context.fill(currPath);
-      context.stroke(currPath);
-      $scope.drawingLine = false;
-      $scope.paths.push(currPath);
-    }
-  };
-
-  $scope.dots = [];
-  $scope.drawDot = function ($event) {
-    if ($scope.drawMode === $scope.drawingModes.DOTS) {
-      var pos = getCursorPosition($event);
-      var r = 2;
-
-      context.beginPath();
-      context.arc(pos.x, pos.y, r, 0, 2 * Math.PI);
-      context.fillStyle = '#000';
-      context.fill();
-      context.stroke();
-
-      $scope.dots.push({x: pos.x, y: pos.y});
-    }
-  };
-
-  $scope.clearCanvas = function () {
-    $scope.dots = [];
-    $scope.paths = [];
-    context.clearRect(0, 0, canvas.width, canvas.height);
-  };
-});
+})(jQuery, Path2D, CustomEvent);
