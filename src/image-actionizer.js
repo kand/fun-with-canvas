eef58a44a4d29fb74286be0f694f39240bcf801b
Andrew Kos <akos123@gmail.com>
2015-07-09 18:51:14 -0500

    Using canvas2svg

--- src/image-actionizer.js
+++ src/image-actionizer.js
@@ -9,7 +9,7 @@ var ImageActionizer = (function (
   var getCursorPosition = function($event, $canvas) {
     var x;
     var y;
-    var canoffset = $(canvas).offset();
+    var canoffset = $canvas.offset();
 
     return {
       x: event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft - Math.floor(canoffset.left),
@@ -18,88 +18,84 @@ var ImageActionizer = (function (
   };
 
   var ImageActionizer = function (imageContainer) {
-    this.imageContainer = imageContainer;
+    this.$imageContainer = $(imageContainer);
 
-    this.currPath = null;
     this.inPath = null;
     this.drawingLine = false;
-
-    this.paths = [];
 
     this.setupElements();
   };
 
   ImageActionizer.prototype.setImage = function (file) {
     this.image.src = URL.createObjectURL(file);
+
+    this.resizeCanvas();
   };
 
   ImageActionizer.prototype.setupElements = function () {
-    this.image = document.createElement('img');
-    this.image.addEventListener('load', this.prepCanvas.bind(this));
-    this.imageContainer.appendChild(this.image);
-  };
-
-  ImageActionizer.prototype.prepCanvas = function () {
-    this.context = new SVGCanvas(this.image.width, this.image.height);
-    this.$canvas = $(this.context.__canvas);
+    this.context = new SVGCanvas();
+    this.$canvas = $(this.context.getSvg());
+    this.$canvas.height(0);
+    this.$canvas.width(0);
 
     this.$canvas.on('mousedown', this.startFreeDraw.bind(this));
     this.$canvas.on('mousemove', this.doFreeDraw.bind(this));
     this.$canvas.on('mouseup', this.stopFreeDraw.bind(this));
 
-    this.imageContainer.appendChild(this.context.__canvas);
+    this.$canvas.on('mousedown', 'path', this.startEditPathDetails.bind(this));
+
+    this.$imageContainer.append(this.$canvas);
+
+    this.image = document.createElement('img');
+    this.image.addEventListener('load', this.resizeCanvas.bind(this));
+    this.$imageContainer.append(this.image);
+  };
+
+  ImageActionizer.prototype.resizeCanvas = function () {
+    this.$canvas.height(this.image.height);
+    this.$canvas.width(this.image.width);
   };
 
   ImageActionizer.prototype.startFreeDraw = function (event) {
     var pos = getCursorPosition(event, this.$canvas);
-    this.currPath = new Path2D();
-    this.currPath.moveTo(pos.x, pos.y);
+    this.context.beginPath();
+    this.context.moveTo(pos.x, pos.y);
     this.drawingLine = true;
   };
 
   ImageActionizer.prototype.doFreeDraw = function (event) {
-    var pos = getCursorPosition(event, this.$canvas);
-    var oldPath = this.inPath;
-
-    this.inPath = -1;
-    for (var i = 0; i < this.paths.length; i++) {
-      if (this.context.isPointInPath(this.paths[i], pos.x, pos.y)) {
-        this.inPath = i;
-      }
-    }
-
-    if (oldPath !== this.inPath) {
-      // fire event to say that path has changed
-      var pathChangedEvent = new CustomEvent(
-        'ImageActionizer:PathChanged', {
-          detail: {
-            oldPath: oldPath,
-            newPath: this.inPath
-          }
-        });
-      this.imageContainer.dispatchEvent(pathChangedEvent);
-    }
-
     if (this.drawingLine) {
-      this.currPath.lineTo(pos.x, pos.y);
-      this.context.stroke(this.currPath);
+      var pos = getCursorPosition(event, this.$canvas);
+      this.context.lineTo(pos.x, pos.y);
+      this.context.stroke();
     }
   };
 
   ImageActionizer.prototype.stopFreeDraw = function (event) {
     if (this.drawingLine) {
-      this.currPath.closePath();
+      this.context.closePath();
       this.context.fillStyle = 'rgba(255, 0, 0, 0.5)';
-      this.context.fill(this.currPath);
-      this.context.stroke(this.currPath);
+      this.context.fill();
+      this.context.stroke();
       this.drawingLine = false;
-      this.paths.push(this.currPath);
     }
   };
 
   ImageActionizer.prototype.clearCanvas = function () {
-    this.paths = [];
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.stopFreeDraw();
+    this.stopEditPathDetails();
+
+    this.context.save();
+    this.$canvas.children('g').empty();
+    this.context.restore();
+  };
+
+  ImageActionizer.prototype.startEditPathDetails = function (e) {
+    this.$imageContainer.trigger('ImageActionizer:StartEditPathDetails', e.target);
+  };
+
+  ImageActionizer.prototype.stopEditPathDetails = function () {
+    this.$imageContainer.trigger('ImageActionizer:StopEditPathDetails');
   };
 
   return ImageActionizer;
